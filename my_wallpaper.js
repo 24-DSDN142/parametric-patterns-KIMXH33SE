1d6184d4b066c882fe48ae911ee921d0ef5af5a3
KIMXH33SE <kimrubi24@gmail.com>
2024-07-25 10:34:44 +1200

    random notes lololol testintesting

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -4,20 +4,21 @@ let rect_height = 20;
 
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
-  pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.output_mode(DEVELOP_GLYPH); // (GRID_WALLPAPER)
+  pWallpaper.resolution(FIT_TO_SCREEN); // SIZE OF SCREEN eg (A3)
+  pWallpaper.show_guide(true); //set this to false when you're ready to print - it's the line guides
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 50;
+  pWallpaper.grid_settings.row_offset  = 50; // try 0, 200
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(240, 255, 240); //light honeydew green colour - use colour picker
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
   rect(40 ,40, rect_width, rect_height);
+  //testing testing jajaja
 }
