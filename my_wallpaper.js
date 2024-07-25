a212ce918a990835f2c42954d28be3ef2fac4d35
KIMXH33SE <kimrubi24@gmail.com>
2024-07-25 13:25:11 +1200

    final sun drawing yay

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -20,11 +20,26 @@ function wallpaper_background() {
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
-  //testing testing jajaja
+  let sunX = 100;
+  let sunY = 100;
+  let sunSize = 70; // variable to change size of circel
+
+  fill(255,204,0); // yellow yolk
+  stroke(255,204,0); // border
+  ellipse(sunX,sunY,sunSize,sunSize); // actual sun
+
+//rays
+  stroke(255,204,0);
+  fill(255,204,0);
+  
+  triangle(100, 25, 95, 55, 105, 55); // Top ray
+  triangle(160, 50, 135, 65, 145, 75); // Top right ray
+  triangle(185, 100, 155, 95, 155, 105); // Right ray
+  triangle(130, 140, 140, 130, 155, 160); // Bott right
+  triangle(100, 175, 95, 145, 105, 145); // Bott ray
+  triangle(70, 135, 60, 130, 50, 155); // bott left
+  triangle(15, 100, 45, 95, 45, 105); // Left ray
+  triangle(40, 50, 65, 65, 55, 75); // Top left 
+
 }
 
-//let iceingSize = 80; {
- // fill(240, 255, 240);
- // ellipse(25,170, iceingSize, iceingSize);
-//}
